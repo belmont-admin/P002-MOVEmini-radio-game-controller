e5deda7fda25b9f1c408c8c17329e7d59180dbd7
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2019-12-25 09:32:01 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    radio.sendString("Stop")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("Right")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    radio.sendString("Stop")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("LEDs Off")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("Left")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("LEDs On")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    radio.sendString("Stop")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    radio.sendString("Stop")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("Forward")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("Backward")
+})
+basic.showLeds(`
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    . . . . .
+    `)
+radio.setGroup(1)
